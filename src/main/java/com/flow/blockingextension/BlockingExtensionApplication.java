6806740283c5f21671978d220fe88250e9f73e82
heaming <meetoohi@gmail.com>
2024-08-13 17:03:04 +0900

    initial setting

---
+++ src/main/java/com/flow/blockingextension/BlockingExtensionApplication.java
@@ -0,0 +1,13 @@
+package com.flow.blockingextension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockingExtensionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockingExtensionApplication.class, args);
+    }
+
+}
